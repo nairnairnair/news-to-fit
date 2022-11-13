212c937d8c4bbe7017f7b4e3298fe2b7dd26b827
emmacintosh <emmammrussell@gmail.com>
2022-11-13 13:39:37 -0700

    Add article view and button to navigate home

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -1,11 +1,29 @@
 import './Article.css'
 
-function Article() {
+function Article({articleClicked, setArticleView}) {
+
+    const returnHandler = () => {
+        setArticleView(false);
+      };
+
+      const arty = articleClicked
+
     return (
       <div className="Article">
-        <header className="App-header">
-        <p>farticle</p>
-        </header>
+        <article className="article-view">
+            <section>
+                <img className="modal-img" src={arty.multimedia[0].url} alt={arty.multimedia[0].caption}/>
+            </section>
+            <h2>{arty.title}</h2>
+            <p>{arty.byline}</p>
+            <p>Published: {arty.published_date}, Updated: {arty.updated_date}</p>
+            <p>{arty.abstract}</p>
+            <p>
+                <a href={arty.url}>Read the full story on The New York Times site.</a>
+                Don't hit the paywall too hard!
+            </p>
+            <button className="returnButton" onClick={returnHandler}>Return</button>
+        </article>
       </div>
     );
   }
